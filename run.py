94c5847344513203c491239fc627fb87e531305b
kthordarson <kthordarson@gmail.com>
2023-02-07 12:30:07 +0100

    guimain

--- run.py
+++ run.py
@@ -4,18 +4,50 @@ from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QDialog, QApplication
+from loguru import logger
+from ui_main import Ui_FindGitsApp
+from dbstuff import GitRepo, GitFolder, send_to_db, get_engine, db_init, send_gitfolder_to_db, get_folder_entries, get_repo_entries
+from utils import get_folder_list
+from sqlalchemy.orm import sessionmaker
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+# from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QSizePolicy, QTreeWidget, QTreeWidgetItem, QWidget)
 
-from app import FindGitsApp, Ui_Form
-
-class graphAnalysis(QDialog, Ui_Form):
-    def __init__(self):
-        super(graphAnalysis, self).__init__()
+class MainApp(QWidget, Ui_FindGitsApp):
+    def __init__(self, session):
+        self.session = session
+        super(MainApp, self).__init__()
         self.setupUi(self)
-        # 点击父节点
         self.treeWidget.itemChanged.connect(self.handleChanged)
+        self.pushButton.clicked.connect(self.on_button_clicked)
+        # self.getgit()
+
+    def getgit(self):
+        repos = session.query(GitRepo).all()
+        # self.populate_gitfolders(repos)
+
+    def populate_gitfolders(self, gitfolders):
+        #self.item_0 = QTreeWidgetItem(self.treeWidget)
+        for k in gitfolders:
+            item_1 = QTreeWidgetItem(self.treeWidget)
+            # item_1.setCheckState(0, QtCore.Qt.Unchecked)
+            item_1.setText(0, f"{k.giturl}")
+        self.retranslateUi(self)
+
+    def on_button_clicked(self, widget):
+        repos = session.query(GitRepo).all()
+        print(f"self.populate_gitfolders(repos) {len(repos)}")
+        self.populate_gitfolders(repos)
 
     def handleChanged(self, item, column):
         count = item.childCount()
+        # logger.debug(f'[h] {self} count={count} i: {item} c: {column}')
         if item.checkState(column) == Qt.Checked:
             for index in range(count):
                 item.child(index).setCheckState(0, Qt.Checked)
@@ -23,12 +55,11 @@ class graphAnalysis(QDialog, Ui_Form):
             for index in range(count):
                 item.child(index).setCheckState(0, Qt.Unchecked)
 
-def xmain():
-	app = QApplication()
-	fgapp = FindGitsApp()
-
 if __name__ == '__main__':
+    engine = get_engine(dbtype='sqlite')
+    Session = sessionmaker(bind=engine)
+    session = Session()
     app = QApplication(sys.argv)
-    w = graphAnalysis()
+    w = MainApp(session)
     w.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
--- ui_main.py
+++ ui_main.py
@@ -11,57 +11,71 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
-    QCursor, QFont, QFontDatabase, QGradient,
-    QIcon, QImage, QKeySequence, QLinearGradient,
-    QPainter, QPalette, QPixmap, QRadialGradient,
-    QTransform)
-from PySide6.QtWidgets import (QApplication, QHeaderView, QMainWindow, QMenu,
-    QMenuBar, QSizePolicy, QStatusBar, QTreeView,
-    QVBoxLayout, QWidget)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QHBoxLayout, QHeaderView,
+    QPushButton, QSizePolicy, QTreeWidget, QTreeWidgetItem,
+    QWidget)
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
-        self.actionQuit = QAction(MainWindow)
-        self.actionQuit.setObjectName(u"actionQuit")
-        self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayoutWidget = QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-        self.verticalLayoutWidget.setGeometry(QRect(19, 9, 761, 541))
-        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.treeView = QTreeView(self.verticalLayoutWidget)
-        self.treeView.setObjectName(u"treeView")
+class Ui_FindGitsApp(object):
+    def setupUi(self, FindGitsApp):
+        if not FindGitsApp.objectName():
+            FindGitsApp.setObjectName(u"FindGitsApp")
+        FindGitsApp.resize(1036, 464)
+        self.treeWidget = QTreeWidget(FindGitsApp)
+        __qtreewidgetitem = QTreeWidgetItem()
+        __qtreewidgetitem.setText(0, u"id");
+        self.treeWidget.setHeaderItem(__qtreewidgetitem)
+        self.treeWidget.setObjectName(u"treeWidget")
+        self.treeWidget.setGeometry(QRect(10, 10, 491, 391))
+        self.treeWidget.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.widget = QWidget(FindGitsApp)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(10, 410, 426, 29))
+        self.horizontalLayout = QHBoxLayout(self.widget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.pushButton = QPushButton(self.widget)
+        self.pushButton.setObjectName(u"pushButton")
 
-        self.verticalLayout.addWidget(self.treeView)
+        self.horizontalLayout.addWidget(self.pushButton)
 
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(MainWindow)
-        self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 24))
-        self.menuFile = QMenu(self.menubar)
-        self.menuFile.setObjectName(u"menuFile")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        self.pushButton_2 = QPushButton(self.widget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
 
-        self.menubar.addAction(self.menuFile.menuAction())
-        self.menuFile.addAction(self.actionQuit)
+        self.horizontalLayout.addWidget(self.pushButton_2)
 
-        self.retranslateUi(MainWindow)
+        self.pushButton_3 = QPushButton(self.widget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        self.horizontalLayout.addWidget(self.pushButton_3)
+
+        self.pushButton_4 = QPushButton(self.widget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+
+        self.horizontalLayout.addWidget(self.pushButton_4)
+
+        self.pushButton_5 = QPushButton(self.widget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+
+        self.horizontalLayout.addWidget(self.pushButton_5)
+
+
+        self.retranslateUi(FindGitsApp)
+
+        QMetaObject.connectSlotsByName(FindGitsApp)
     # setupUi
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.actionQuit.setText(QCoreApplication.translate("MainWindow", u"Quit", None))
-        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
+    def retranslateUi(self, FindGitsApp):
+        FindGitsApp.setWindowTitle(QCoreApplication.translate("FindGitsApp", u"Form", None))
+        ___qtreewidgetitem = self.treeWidget.headerItem()
+        ___qtreewidgetitem.setText(1, QCoreApplication.translate("FindGitsApp", u"url", None));
+        self.pushButton.setText(QCoreApplication.translate("FindGitsApp", u"populate", None))
+        self.pushButton_2.setText(QCoreApplication.translate("FindGitsApp", u"populate", None))
+        self.pushButton_3.setText(QCoreApplication.translate("FindGitsApp", u"populate", None))
+        self.pushButton_4.setText(QCoreApplication.translate("FindGitsApp", u"populate", None))
+        self.pushButton_5.setText(QCoreApplication.translate("FindGitsApp", u"populate", None))
     # retranslateUi
 
