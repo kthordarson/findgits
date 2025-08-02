3f46837f0133ab3a33300c3e74a6d4a87ead275c
kthordarson <kthordarson@gmail.com>
2025-08-02 16:04:17 +0000

    del views

--- dbviews.py
+++ dbviews.py
@@ -48,19 +48,3 @@ def _create_view(element, compiler, **kw):
 @compiler.compiles(DropView)
 def _drop_view(element, compiler, **kw):
 	return f"DROP VIEW {element.name}"
-
-
-def view_exists(ddl, target, connection, **kw):
-	return ddl.name in sa.inspect(connection).get_view_names()
-
-
-def view_doesnt_exist(ddl, target, connection, **kw):
-	return not view_exists(ddl, target, connection, **kw)
-
-
-def view(name, metadata, selectable):
-	t = table(name)
-	t._columns._populate_separate_keys(col._make_proxy(t) for col in selectable.selected_columns)
-	sa.event.listen(metadata, "after_create", CreateView(name, selectable).execute_if(callable_=view_doesnt_exist), )
-	sa.event.listen(metadata, "before_drop", DropView(name).execute_if(callable_=view_exists))
-	return t
