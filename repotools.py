c7bc266d33c56961e091c432eb2dd8069422fe67
kthordarson <kthordarson@gmail.com>
2025-08-04 10:28:36 +0000

    fix insert_update_starred_repo

--- repotools.py
+++ repotools.py
@@ -282,22 +282,22 @@ async def insert_update_starred_repo(github_repo, session, args, create_new=Fals
 			if git_list:
 				git_star.gitlist_id = git_list.id
 				logger.info(f"Linked GitStar {git_star.id} to GitList {git_list.id} ({list_name})")
-		else:
-			# Try to determine which list this repo belongs to
-			git_lists_data = await get_git_list_stars(session, args)
-			repo_full_name = git_repo.full_name or full_name
+		# else:
+		# 	# Try to determine which list this repo belongs to
+		# 	git_lists_data = await get_git_list_stars(session, args)
+		# 	repo_full_name = git_repo.full_name or full_name
 
-			for list_name_check, list_data in git_lists_data.items():
-				for href in list_data.get('hrefs', []):
-					href_clean = href.strip('/').split('github.com/')[-1].rstrip('.git')
-					if href_clean == repo_full_name:
-						git_list = session.query(GitList).filter(GitList.list_name == list_name_check).first()
-						if git_list:
-							git_star.gitlist_id = git_list.id
-							logger.info(f"Auto-linked GitStar {git_star.id} to GitList {git_list.id} ({list_name_check})")
-						break
-				if git_star.gitlist_id:
-					break
+		# 	for list_name_check, list_data in git_lists_data.items():
+		# 		for href in list_data.get('hrefs', []):
+		# 			href_clean = href.strip('/').split('github.com/')[-1].rstrip('.git')
+		# 			if href_clean == repo_full_name:
+		# 				git_list = session.query(GitList).filter(GitList.list_name == list_name_check).first()
+		# 				if git_list:
+		# 					git_star.gitlist_id = git_list.id
+		# 					logger.info(f"Auto-linked GitStar {git_star.id} to GitList {git_list.id} ({list_name_check})")
+		# 				break
+		# 		if git_star.gitlist_id:
+		# 			break
 	else:
 		logger.warning(f'No GitRepo found for remote_url: {remote_url}')
 
